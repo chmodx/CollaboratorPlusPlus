f5b4135837edeb3b1f26f1a8a03c0b2b3573d683
Ali Huseyn Aliyev <ali.a.huseyn@gmail.com>
2020-08-04 16:21:22 +0300

    Update startcollab.sh

--- startcollab.sh
+++ startcollab.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar /usr/local/collaboratorplusplus/CollaboratorPlusPlus.jar CollaboratorServer.propertie
+java -jar /usr/local/collaboratorplusplus/CollaboratorPlusPlus.jar /usr/local/collaboratorplusplus/CollaboratorServer.properties
